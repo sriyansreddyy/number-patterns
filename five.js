aba54f1e8ab76d48dd2f7e1d1a536015c1d0034a
Sriyans Reddy <80452443+sriyansreddyy@users.noreply.github.com>
2021-06-16 21:15:17 +0530

    multiples of numbers 2, 3, 4, 5 upto 20x

---
+++ five.js
@@ -0,0 +1,27 @@
+console.log("Multuples of 2:")
+for(let i = 1; i <= 40; i++) {
+    if(i % 2 ==0) {
+        console.log(i)
+    }
+}
+
+console.log("Multiples of 3:")
+for( let i=1; i<=60; i++){ 
+  if(i % 3 == 0){
+     console.log(i);
+   }
+ }
+ 
+ console.log("Multiples of 4:")
+for( let i=1; i<=80; i++){ 
+  if(i % 4 == 0){
+     console.log(i);
+   }
+ }
+ 
+ console.log("Multiples of 5:")
+for( let i=1; i<=100; i++){ 
+  if(i % 5 == 0){
+     console.log(i);
+   }
+ }
